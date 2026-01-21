186f4c46056f281d20645f50027ee4a3a831848d
Aditya Dubey <adityadubey2433@gmail.com>
2026-01-21 22:23:20 +0530

    keywords

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 123456
+let accountEmail = "aditya@22googal.com"
+var accountPassword = "1235"
+accountCity = "Majhwaliya"
+
+// accountId = 2 // it is not be change 
+
+accountEmail = "adi@googal.com"
+accountPassword = "587456"
+accountCity = "Vijaipur"// we can defined it without variable keywords 
+
+
+let accountState; // it will show undefine becouse uski value nhi diya  hai 
+
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+/*
+prefer not to use var
+becouse  of block scope , functional scope problem
+*/
