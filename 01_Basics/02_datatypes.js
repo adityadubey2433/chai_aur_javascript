ac2b47c449dedaf8a7e864970721d2aa7c19cd20
Aditya Dubey <adityadubey2433@gmail.com>
2026-01-21 23:57:51 +0530

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,36 @@
+"use strict"; //treat all JS code as newer version
+
+// alert (3 + 3) // we are using nodejs not browser
+
+console.log(3 + 3); 
+                        //ye code likhne ke liye sahi tarika hai 
+ console.log("Aditya")
+
+      //or//  
+
+console.log("Aditya"); console.log("Dubey") // aise v code likh sakte hai      
+
+
+let name ="Aditya"
+let age = "21"
+let isloggedin = false
+
+let state; // => undefined
+
+
+
+//it is datatypes in js
+//number => 2 to power 53
+//bigint =>
+//sting => ""
+//boolean => true/false
+//null => standalone value
+//undefined => variable decleared but not assigned 
+//Symbol => unique identifire
+//objects => 
+
+
+
+    console.log(typeof"Aditya");
+    console.log(typeof null); // type of null is object
+    console.log(typeof undefined); // and type of undefinned is undefined
