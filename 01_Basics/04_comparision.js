50ca676f9f6af4ebca4369f8acb303e79c7c14f7
Aditya Dubey <adityadubey2433@gmail.com>
2026-01-22 23:58:50 +0530

    hello

--- 01_Basics/04_comparision.js
+++ 01_Basics/04_comparision.js
@@ -5,11 +5,13 @@ console.log(2 <= 1);
 console.log(2 != 1);
 
 
-console.log("2" > 1);
+console.log("2" > 1); // problem start here due to defferent data types
 console.log("02" > 1);
-
+// iss tarah ke conversion ko hamesha avoid krna chahiye.
 
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
 // the reason is that an equality check == and comparison > < >= <= work diffrently.  Comparison conversion null to a number. treating it as 0. That's why (3) null >= 0 is true and (1) null > 0 is false.
+
+console.log("2" === 2); // strictly check datatype also
