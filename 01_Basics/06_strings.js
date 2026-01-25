354a5c616a583ea77dfc3b5f3c6b28b8e40aa267
Aditya Dubey <adityadubey2433@gmail.com>
2026-01-25 22:15:01 +0530

    srting

---
+++ 01_Basics/06_strings.js
@@ -0,0 +1,16 @@
+const name = "Aditya"
+const repoCount = 25
+
+// console.log(name + repoCount + " Value"); aise code  kam use krte hai
+
+console.log(`Hello my name is ${name} and my reo count is ${repoCount}`); // ( ``:- iska name backtick hai)  
+
+const gameName = new String('AdityaAND')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toLocaleUpperCase());
+
+
