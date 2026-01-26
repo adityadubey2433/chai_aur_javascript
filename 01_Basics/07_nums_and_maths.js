70b8b23434dd7c2ef0fdde05191018a6e93fce6b
Aditya Dubey <adityadubey2433@gmail.com>
2026-01-26 21:25:03 +0530

    math

--- 01_Basics/07_nums_and_maths.js
+++ 01_Basics/07_nums_and_maths.js
@@ -1,22 +1,38 @@
 const score = 500
-console.log(score);
+// console.log(score);
 
 const balance = new Number(200)
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString());
-console.log(balance.toString().length);
-console.log(balance.toFixed(3));
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
 
  const otherNumber = 25.6589 // 25.7
 // const otherNumber = 125.6589 // 126
 // const otherNumber = 1125.6589 // 1.13e+3   (note:- 1.13 * 10^3)
 
-console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(3));
 
 const hundreds = 1000000
-console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-IN'));
 // console.log(hundreds.toLocaleString('en-US'));
 // console.log(hundreds.toLocaleString('en-CH'));
 // console.log(hundreds.toLocaleString('en-GK'));
 // console.log(hundreds.toLocaleString('en-NP'));
+
+
+
+
+// ____________________________________________ Maths_______________________________________________________________________
+
+console.log(Math); //Object [Math] {}
+
+// console.log(Math.abs(-5)); // negetive value ko positive me convort krta hai 
+// console.log(Math.abs(5)); // positive  value ko positive me convort krta hai
+
+console.log(Math.round(5.5));
+console.log(Math.ceil(5.2));
+console.log(Math.floor(5.6));
+console.log(Math.round(5.3));
+
